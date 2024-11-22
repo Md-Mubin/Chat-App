56cf0a7191ca5b9cd982d6f6e001463ab084b753
Md Mubin <fontdevmubin@gmail.com>
2024-11-22 22:48:12 +0600

    Changes to be committed: 	modified:   src/Components/UserProfile/Userprofile.jsx

--- src/Components/UserProfile/Userprofile.jsx
+++ src/Components/UserProfile/Userprofile.jsx
@@ -12,12 +12,12 @@ const Userprofile = () => {
                 <div className="container">
                     <ul className='userCart'>
                         <ul className='userProfileHead'>
-                            <img src={userData?.photoURL} alt="image" className='m-auto' />
-                            <li>{userData?.displayName}</li>
+                            <img src={userData.photoURL} alt="image" className='m-auto' />
+                            <li>{userData.displayName}</li>
                         </ul>
                         <ul className='userInfosCol'>
-                            <li><span>Name: </span> {userData?.displayName}</li>
-                            <li><span>Email:</span> {userData?.email}</li>
+                            <li><span>Name: </span> {userData.displayName}</li>
+                            <li><span>Email:</span> {userData.email}</li>
                             <li><span>Address:</span> </li>
                             <li><span>Age:</span> </li>
                             <li><span>Contact:</span> </li>
